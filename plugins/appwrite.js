7f0ba9a6472f7ba11bb0645d6f9599cb46287bfd
João Pedro Antunes Silva <joao-pedroas@hotmail.com>
2021-05-30 17:29:22 -0300

    feat(plugins): add appwrite plugin

--- plugins/appwrite.js
+++ plugins/appwrite.js
@@ -1,10 +1,13 @@
 import { Appwrite } from "appwrite";
 
-// Init your Web SDK
-const appwrite = new Appwrite();
 
-appwrite
+export default (ctx, inject) => {
+  // Init your Web SDK
+  const appwrite = new Appwrite();
+
+  appwrite
     .setEndpoint('https://aploscreative.dev/v1') // Set only when using self-hosted solution
     .setProject('60aeec8c93f11');
 
-export default appwrite;
+  inject('appwrite', appwrite)
+}
